9a41d6c5f8d0f14925eb9ca03d720f59264919f5
yizhakg <yizhakg.gmail.com>
2021-03-25 03:02:31 +0200

    drage sliders

--- client/src/components/uiComponents/slideFlex/SlideFlex.jsx
+++ client/src/components/uiComponents/slideFlex/SlideFlex.jsx
@@ -7,26 +7,35 @@ export default function SlideFlex({ item }) {
     return < img key={`img${i}`} className="playlistImg" src={playlist.playlistImage} alt="" />
   })
   const handleClick = (e) => {
-    e.target.onmousedown = (event) => {
-      event.target.onmousemove = (event2) => {
-        console.log(event2)
-      }
-    }
-    e.target.onmouseup = (event) => {
-      event.target.onmousemove = null
+    const ele = e.target;
+    const pos = {
+      // The current scroll 
+      left: ele.scrollLeft,
+      // Get the current mouse position
+      x: e.clientX,
+    };
 
-    }
+    const mouseMoveHandler = function (e) {
+      // How far the mouse has been moved
+      const dx = e.clientX - pos.x;
+
+      // Scroll the element
+      ele.scrollLeft = pos.left - dx;
+
+    };
+
+    ele.onmousemove = mouseMoveHandler
+  }
+  const handleUp = (e) => {
     e.target.onmousemove = null
-
-
   }
 
 
   return (
 
     <React.Fragment>
-      <div className="slide-wrapper" onMouseEnter={handleClick}>
-        <div className="slide">
+      <div className="slide-wrapper" >
+        <div className="slide" id="slide" onMouseDown={handleClick} onMouseUp={handleUp}>
           {Slides}
         </div>
       </div>
